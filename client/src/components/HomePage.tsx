525ab735146be32b3a21de7c868d8d3cb598e941
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-03-15 03:15:22 +0000

    creating the landing page

---
+++ client/src/components/HomePage.tsx
@@ -0,0 +1,12 @@
+import { WavyBackground } from "./wavy-background";
+
+
+function HomePage() {
+  return (
+    <WavyBackground className="max-w-4xl mx-auto pb-40">
+      
+    </WavyBackground>
+  )
+}
+
+export default HomePage
---
+++ client/src/components/LandingPage.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import { WavyBackground } from "./wavy-background";
+import "../styles/LandingPage.css";
+
+const LandingPage = () => {
+    return (
+        <WavyBackground className="max-w-4xl mx-auto pb-40">
+            <div className="text-center">
+                <p className="text-6xl md:text-8xl lg:text-9xl text-white font-bold inter-var">
+                    UpBeat
+                </p>
+                <p className="text-base md:text-lg mt-4 text-white font-normal inter-var">
+                    Dance To Your Own Beat With UpBeat
+                </p>
+                <div className="link-container hover:scale-110">
+                    <Link
+                        to="/home"
+                        className="landing-page-link text-base text-white font-bold">
+                        Tune In
+                    </Link>
+                </div>
+            </div>
+        </WavyBackground>
+    );
+};
+
+export default LandingPage;
