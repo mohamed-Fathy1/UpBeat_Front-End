b26fac51b193d824dbe028e21f420c20ca98ecfe
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-03-18 10:46:02 +0200

    change vite server config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+<<<<<<< Updated upstream
+        host: '127.0.0.1',
+=======
+        host: '127.0.0.1'
+>>>>>>> Stashed changes
+    }
 })
