a3ea5f394204e694b916120550cec5de40705d87
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-03-23 23:50:51 +0000

    fixing typos after resolving the conflict

--- client/src/components/TrackCard/TrackCard.tsx
+++ client/src/components/TrackCard/TrackCard.tsx
@@ -6,20 +6,20 @@ import playerContext from "../../context/playerContext";
 
 function TrackCard({ id, link, image, artist, title, alt }: Song) {
   const { setCurrentSong, currentSong } = useContext(playerContext);
-  
+
   return (
     <li
       onClick={() => setCurrentSong(id)}
-      className={`content-item border-2 w-40 ${
-        currentSong === id ? "border-green-500" : "border-transparent"
-      }`}
+      className={`content-item bg-transparent border-2 w-40 ${currentSong === id ? "border-green-500" : "border-transparent"
+        }`}
     >
       <Link to={""} className="content-item">
         <img src={image} alt={alt} className="rounded-sm" />
         <h3>{artist}</h3>
         <h5>{title}</h5>
       </Link>
-    
+    </li>
+
   );
 }
 
